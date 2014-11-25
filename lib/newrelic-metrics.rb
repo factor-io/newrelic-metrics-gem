0b4b78600b0b683a0d02110e3f4fc03dadb5975f
Maciej Skierkowski <maciej@skierkowski.com>
2014-11-25 10:18:59 -0800

    Improving error handling and config

--- lib/newrelic-metrics.rb
+++ lib/newrelic-metrics.rb
@@ -3,63 +3,102 @@ require 'json'
 require 'uri'
 require 'chronic'
 
-class NewRelicMetrics
-  BASE = 'https://api.newrelic.com/v2'
-
-  def initialize(api_key,ids={})
-    @api_key = api_key
-    raise ArgumentError, "Need to define either an application or server id" if ids.keys.length!=1
-    raise ArgumentError, "#{ids.keys.first} must be `application` or `server`" if ids.keys.first != :server && ids.keys.first != :application
-    @resource = "#{ids.keys.first}s"
-    @resource_id = ids.values.first
+module NewRelicMetrics
+  class Configuration
+    attr_accessor :api_key
+    def initialize(api_key:nil,&block)
+      @api_key=api_key
+      yield(self) if block
+    end
   end
 
-  def names
-    get("metrics")
+  class << self
+    attr_accessor :configuration
   end
 
-  def summarize(metrics={},range={})
-    throw ArgumentError, "Range must be an array with the from and to time" if range.length>2
-
-    settings = {summarize:true}.merge(range)
-
-    metrics(metrics,settings)
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
   end
 
-  def metrics(metrics={},options={})
-    query = ""
-    conditions = []
+  class RequestFailed < StandardError
+  end
 
-    names = metrics.keys
-    values = metrics.values.flatten
+  class Client
+    BASE = 'https://api.newrelic.com/v2'
 
-    names.each {|name| conditions << "names[]=#{URI.encode(name)}" }
-    values.each {|val| conditions << "values[]=#{URI.encode(val)}" }
+    def initialize(config=nil)
+      @config = config || NewRelicMetrics.configuration
 
-    if options[:from]
-      from_time = Chronic.parse(options[:from], context: :past)
-      to_time = Chronic.parse(options[:to]) if options[:to]
-      to_time ||= Time.now
-      if from_time
-        conditions << "from=#{from_time.getlocal('+00:00').iso8601}"
-        conditions << "to=#{to_time.getlocal('+00:00').iso8601}"
-      end
+      raise ArgumentError, "No API Key is configured" unless @config && @config.api_key
     end
 
-    conditions << "summarize=true" if options[:summarize]
+    def names
+      get("metrics")
+    end
 
-    query = conditions.join('&')
-    get("metrics/data",query)
-  end
+    def summarize(application:nil, server:nil, metrics:, range:{})
 
-  private
+      if range && range!={}
+        throw ArgumentError, "Range must only contain a :to and :from time" if range.keys.length != 2
+        throw ArgumentError, "Range must contain a from time" if range.include?(:from)
+        throw ArgumentError, "Range must contain a to time" if range.include?(:to)
+      end
 
-  def get(path,query=nil)
-    uri = URI.parse('https://api.newrelic.com/')
-    uri.path = "/v2/#{@resource}/#{@resource_id}/#{path}.json"
-    uri.query = query if query && query != ""
+      raise ArgumentError, "Need to define either an application or server id" unless application || server
+      raise ArgumentError, "Need to define either an application or server id, but not both" if application && server
 
-    response = RestClient.get(uri.to_s,'X-Api-Key'=>@api_key)
-    JSON.parse(response).values.first
+      @resource = application ? 'applications' : 'servers'
+      @resource_id = application || server
+
+      settings = {summarize:true}.merge(range)
+
+      metrics(metrics,settings)
+    end
+
+    def metrics(metrics={},options={})
+      query = ""
+      conditions = []
+
+      names = metrics.keys
+      values = metrics.values.flatten
+
+      names.each {|name| conditions << "names[]=#{URI.encode(name)}" }
+      values.each {|val| conditions << "values[]=#{URI.encode(val)}" }
+
+      if options[:from]
+        from_time = Chronic.parse(options[:from], context: :past)
+        to_time = Chronic.parse(options[:to]) if options[:to]
+        to_time ||= Time.now
+        if from_time
+          conditions << "from=#{from_time.getlocal('+00:00').iso8601}"
+          conditions << "to=#{to_time.getlocal('+00:00').iso8601}"
+        end
+      end
+
+      conditions << "summarize=true" if options[:summarize]
+
+      query = conditions.join('&')
+      get("metrics/data",query)
+    end
+
+    private
+
+    def get(path,query=nil)
+      uri = URI.parse('https://api.newrelic.com/')
+      uri.path = "/v2/#{@resource}/#{@resource_id}/#{path}.json"
+      uri.query = query if query && query != ""
+
+      begin
+        response = RestClient.get(uri.to_s,'X-Api-Key'=>@config.api_key)
+      rescue => ex
+        if ex.response
+          message = JSON.parse(ex.response).values.first['title']
+          raise RequestFailed, message
+        else
+          raise RequestFailed, ex.message
+        end
+      end
+    end
   end
 end
