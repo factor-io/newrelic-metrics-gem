ac0f20ea67fc3bb08e309d5ca30a6d468c6376cd
Maciej Skierkowski <maciej@skierkowski.com>
2014-12-04 15:01:45 -0800

    Adding configuration and initialization specs

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,5 +2,42 @@ require 'spec_helper'
 
 describe NewRelicMetrics do
   describe NewRelicMetrics::Configuration do
+    before do
+      @api_key = ENV['NEWRELIC_API_KEY']
+    end
+
+    after do
+      NewRelicMetrics.configuration = nil
+    end
+
+    it 'can set global config' do
+      NewRelicMetrics.configure do |c|
+        c.api_key = @api_key
+      end
+
+      expect(NewRelicMetrics.configuration.api_key).to eq(ENV['NEWRELIC_API_KEY'])
+    end
+
+    it 'can set config using a method' do
+      config = NewRelicMetrics::Configuration.new
+
+      config.api_key = @api_key
+
+      expect(config.api_key).to eq(@api_key)
+    end
+
+    it 'can set config with a block' do 
+      config = NewRelicMetrics::Configuration.new do |c|
+        c.api_key = @api_key
+      end
+
+      expect(config.api_key).to eq(@api_key)
+    end
+
+    it 'can set config with a hash' do 
+      config = NewRelicMetrics::Configuration.new(api_key: @api_key)
+
+      expect(config.api_key).to eq(@api_key)
+    end
   end  
 end
--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -2,5 +2,21 @@ require 'spec_helper'
 
 describe NewRelicMetrics do
   describe NewRelicMetrics::Client do
+    before do
+      @api_key = ENV['NEWRELIC_API_KEY']
+    end
+
+    describe 'Initialization' do
+      it 'can throws if no API Key set' do
+        
+        expect { NewRelicMetrics::Client.new }.to raise_error(ArgumentError)
+
+      end
+
+      it 'can initialize with a config class' do
+        config = NewRelicMetrics::Configuration.new(api_key: @api_key)
+        expect { NewRelicMetrics::Client.new(config) }.not_to raise_error        
+      end
+    end
   end  
 end
