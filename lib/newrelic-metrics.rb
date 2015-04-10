6aaa61b90830047cfd12d4e069ea3e26a090d048
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-10 15:46:19 -0700

    a little bit of refactoring

--- lib/newrelic-metrics.rb
+++ lib/newrelic-metrics.rb
@@ -33,44 +33,36 @@ module NewRelicMetrics
       raise ArgumentError.new("No API Key is configured") unless @config && @config.api_key
     end
 
-    def names(options)
-      application = options[:application]
-      server      = options[:server]
-      raise ArgumentError.new("Need to define either an application or server id") unless application || server
-      raise ArgumentError.new("Need to define either an application or server id, but not both") if application && server
-      resource = application ? 'applications' : 'servers'
-      resource_id = application || server
+    def names(options={})
+      resource_id = options[:application] || options[:server]
+      resource    = options[:application] ? 'applications' : 'servers'
+      raise ArgumentError.new("Need to define either an application or server id") unless resource_id
+      raise ArgumentError.new("Need to define either an application or server id, but not both") if options[:application] && options[:server]
+      
       get(resource, resource_id, "metrics")['metrics']
     end
 
-    def metrics(options)
-      application = options[:application]
-      server      = options[:server]
+    def metrics(options={})
+      resource_id = options[:application] || options[:server]
+      resource    = options[:application] ? 'applications' : 'servers'
       metrics     = options[:metrics]
       range       = options[:range] || {}
       summarize   = options[:summarize] || false
+      conditions  = []
 
-      raise ArgumentError.new("missing keyword: metrics") unless metrics
-
-      if range && range!={}
-        raise ArgumentError.new("Range must only contain a :to and :from time") unless range.keys.all?{|k| k==:to || k==:from }
-        raise ArgumentError.new("Range must contain a :from time") unless range.keys.include?(:from)
-      end
-
-      raise ArgumentError.new("Need to define either an application or server id") unless application || server
-      raise ArgumentError.new("Need to define either an application or server id, but not both") if application && server
-
-      raise ArgumentError.new("Metrics must be set") if !metrics || metrics=={}
+      raise ArgumentError.new("Need to define either an application or server id") unless resource_id
+      raise ArgumentError.new("Need to define either an application or server id, but not both") if options[:application] && options[:server]
+      raise ArgumentError.new("Metrics must be set") unless metrics
       raise ArgumentError.new("Metrics must be an hash") unless metrics.is_a?(Hash)
       raise ArgumentError.new("Metric keys must be string") unless metrics.keys.all?{|k| k.is_a?(String)}
       raise ArgumentError.new("Metric values must be arrays") unless metrics.values.all?{|k| k.is_a?(Array)}
       raise ArgumentError.new("Metric values must be an array of strings") unless metrics.values.all?{|k| k.all?{|v| v.is_a?(String)} }
 
-      resource = application ? 'applications' : 'servers'
-      resource_id = application || server
-
-      conditions = []
-
+      if range && range!={}
+        raise ArgumentError.new("Range must only contain a :to and :from time") unless range.keys.all?{|k| k==:to || k==:from }
+        raise ArgumentError.new("Range must contain a :from time") unless range.keys.include?(:from)
+      end
+  
       metrics.keys.each {|name| conditions << "names[]=#{URI.encode(name)}" }
       metrics.values.flatten.each {|val| conditions << "values[]=#{URI.encode(val)}" }
 
@@ -100,13 +92,10 @@ module NewRelicMetrics
       begin
         response = RestClient.get(uri.to_s,'X-Api-Key'=>@config.api_key)
       rescue => ex
-        if ex.response
-          message = JSON.parse(ex.response).values.first['title']
-          raise RequestFailed, message
-        else
-          raise RequestFailed, ex.message
-        end
+        message = ex.response ? JSON.parse(ex.response).values.first['title'] : ex.message
+        raise RequestFailed, message
       end
+
       begin
         content = JSON.parse(response)
       rescue => ex
